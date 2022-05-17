53423956fa2c19defacd9294f727308c0ff2ccba
SteinX <steinxia@gmail.com>
2022-05-18 00:51:17 +0800

    feat: personalize dotfiles

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,46 @@
 #!/usr/bin/env bash
 
-cd "$(dirname "${BASH_SOURCE}")";
-
 git pull origin main;
 
 function doIt() {
+	if [ ! -x "$(command -v brew)" ]; then
+		echo "Homebrew cannot be found, get it installed first"
+		if ! /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"; then
+			echo "Homebrew cannot be installed"
+			exit 1
+		fi
+	fi
+
+	echo "Fetch all deps from Brewfile"
+	if ! brew bundle install; then
+		echo "Something went wrong in installing deps from Brewfile"
+	fi
+
+	if [ -x "$(command -v zsh)" ]; then
+		echo "We have zsh installed, and the following add-ons will be settled as well: on-my-zsh & powerlevel10k-theme"
+		# oh-my-zsh
+		if [ -x "$(command -v curl)" ]; then
+			sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	  elif [ -x "$(command -v wget)" ]; then
+			sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+		else
+			echo "Neither curl nor wget is available for fetching the source of oh-my-zsh"
+		fi
+
+		# Powerlevel10k
+		git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+	fi
+
 	rsync --exclude ".git/" \
 		--exclude ".DS_Store" \
 		--exclude ".osx" \
+		--exclude ".macos" \
 		--exclude "bootstrap.sh" \
+		--exclude "Brewfile" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
+	source ~/.zshrc
 	source ~/.bash_profile;
 }
 
